005f81dd397db6a72d9afe2d882e9d0cd0df46e5
MarcSerraPeralta <43704266+MarcSerraPeralta@users.noreply.github.com>
2024-09-02 16:58:40 +0200

    Create setup_git.sh

---
+++ setup_git.sh
@@ -0,0 +1,6 @@
+# enable git autocompletion
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+chmod +x ~/.git-completion.bash
+echo "if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi" >> ~/.bash_profile
